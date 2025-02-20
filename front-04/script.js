791faadea7d6c052b32a72266a22b9a93ee2bd2e
Vinix-180hz <gkvinicius@gmail.com>
2025-02-20 08:25:12 -0300

    Adicionando

--- front-04/script.js
+++
@@ -1,2 +0,0 @@
-//alert("olá mundo");
-console.log("Olá Pedro");
---
+++ front-04/variavel.js
@@ -0,0 +1,8 @@
+let nome = "Pedro"; // String
+let idade = 16; // Int
+let casado = false; // Booleano
+
+//Imprimindo as variáveis
+console.log( typeof nome);  
+console.log( typeof idade);
+console.log( typeof casado);
