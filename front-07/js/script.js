2b92c20a1ebc8824e547a9c1dd547d6790d434d7
Vinix-180hz <gkvinicius@gmail.com>
2025-03-20 09:56:10 -0300

    adicionando arquivos

---
+++ front-07/js/script.js
@@ -0,0 +1,21 @@
+const nome = document.querySelector("nome");
+const button = document.querySelector("button");
+const lista = document.querySelector(".lista");
+
+// Função Limpar campo
+function clearInput() {
+    nome.value = "";
+}
+
+// Criando uma Função
+button.addEventListener("click", function(event) {
+    event.preventDefault();
+    const inputValue = nome.value;
+    const templateHTML = `<li>${inputValue}</li>`;
+
+    // Adicionando lista <LI>
+    lista.innerHTML  += templateHTML;
+    
+    // Limpando Campo
+    clearInput();
+});
