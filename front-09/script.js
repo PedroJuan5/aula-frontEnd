15ef2c7d36bb0c31c074f2aa3e9b4bd6d71aa5fb
Vinix-180hz <gkvinicius@gmail.com>
2025-04-10 10:46:36 -0300

    adicionando novos arquivos

--- front-09/script.js
+++ front-09/script.js
@@ -1,24 +1,66 @@
 const cars = [
     {
         brand: "Toyota",
-        model: "corolla",
-        yaer: 2020
+        model: "Corolla",
+        year: 2020
     },
     {
         brand: "Honda",
-        model: "civic",
-        yaer: 2019
+        model: "Civic",
+        year: 2019
     },
     {
         brand: "Ford",
-        model: "mustang",
-        yaer: 2022
+        model: "Fiesta",
+        year: 2014
     },
     {
         brand: "Ford",
-        model: "Ka",
-        yaer: 2018
+        model: "Focus",
+        year: 2018
+    },
+    {
+        brand: "Fiat",
+        model: "Uno",
+        year: 2015
+    },
+    {
+        brand: "Volkswagen",
+        model: "Jetta",
+        year: 2022
     }
 ];
-
-/* Criando Fumção */
+ 
+console.log(cars [0].brand);
+console.log(cars [2].model);
+ 
+/* Criando uma função */
+function displayCars() {
+    // Variável
+    const cardList = document.querySelector("#card-list");
+ 
+    cars.forEach((car) => {
+        console.log(car);
+        // Criamos um elemnto div pelo JavaScript
+        const cardDiv = document.createElement("div");
+        //  criando uma classe css car
+        cardDiv.classList.add("car");
+ 
+        // Criando o modelo
+        const cardModel = document.createElement("h2");
+        cardModel.textContent = `${car.brand} ${car.model}`;
+ 
+        //Criando o Ano
+        const cardYear = document.createElement("p");
+        cardYear.textContent = `Ano: ${car.year}`;
+ 
+       
+        cardModel.appendChild(cardYear);
+        cardDiv.appendChild(cardModel);
+        cardList.appendChild(cardDiv);
+       
+    });
+}
+ 
+// Chama a função
+displayCars();
