45efc29f04d933e92d60a698f0c7678cc1791f97
Vinix-180hz <gkvinicius@gmail.com>
2025-04-10 08:52:34 -0300

    adiconando arquivos

---
+++ front-09/script.js
@@ -0,0 +1,24 @@
+const cars = [
+    {
+        brand: "Toyota",
+        model: "corolla",
+        yaer: 2020
+    },
+    {
+        brand: "Honda",
+        model: "civic",
+        yaer: 2019
+    },
+    {
+        brand: "Ford",
+        model: "mustang",
+        yaer: 2022
+    },
+    {
+        brand: "Ford",
+        model: "Ka",
+        yaer: 2018
+    }
+];
+
+/* Criando Fumção */
