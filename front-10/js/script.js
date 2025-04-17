6a2b67b8102c3098d7bc64879447c19beb9feeb3
Vinix-180hz <gkvinicius@gmail.com>
2025-04-17 10:53:01 -0300

    adicionando arquivos

--- front-10/js/script.js
+++ front-10/js/script.js
@@ -1,1 +1,31 @@
-console.log("Olá SESI");
+function fetchObjetos() {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+    .then((resposta) => resposta.json())
+    .then((posts) => {
+        const postContainer = document.getElementById("posts");
+
+
+        posts.forEach((post) => {
+            console.log(post.title)
+            const postDiv = document.createElement("div");
+            postDiv.classList.add("post");
+
+            //Montar um h2 -> title
+            const postTitle = document.createElement("h2")
+            postTitle.textContent = "Bangu";
+
+            // Montar a tag p e buscar o body
+            const postBody =  document.createElement("p");
+            postBody.textContent = post.body;
+
+            //Adicionar estrura na tela
+            // Chamar estrutura HTML
+            postDiv.appendChild(postTitle);
+
+            postContainer.appendChild(postDiv);
+        });
+    });
+}
+
+//chamando a função
+fetchObjetos();
